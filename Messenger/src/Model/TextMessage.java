f0903f5e16b712e68c191fd118b24a8e6542391a
Mallevs1 <Patrik.brandell@hotmail.com>
2022-05-01 21:05:56 +0200

    messageobject

--- Messenger/src/Model/TextMessage.java
+++ Messenger/src/Model/TextMessage.java
@@ -1,4 +1,39 @@
 package Model;
 
+import org.w3c.dom.Text;
+
+import javax.swing.*;
+
 public class TextMessage {
+
+    private String message;
+    private Icon icon;
+
+    public TextMessage (String message, Icon icon) {
+        this.message = message;
+        this.icon = icon;
+    }
+    public TextMessage (String message) {
+        this.message = message;
+    }
+
+    public TextMessage (Icon icon) {
+        this.icon = icon;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Icon getIcon() {
+        return icon;
+    }
+
+    public void setIcon(Icon icon) {
+        this.icon = icon;
+    }
 }
